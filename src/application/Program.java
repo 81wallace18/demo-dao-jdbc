17fe0f6e59bbf49e9edfe66b5c95e9827027132f
Wallace Patrick <patrickwallace621@gmail.com>
2023-08-30 15:49:26 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,17 @@
 package application;
 
+import java.text.ParseException;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws ParseException {
+		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(12, "Wallace", "wallace@gmail.com", new Date(), 1200.0, obj);
+		System.out.println(seller);
 	}
 }
